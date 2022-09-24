66a685704236f6cc32dc24bf3c2ad29a3695dc4b
Noah van der Aa <ndvdaa@gmail.com>
2022-09-24 16:23:48 +0200

    Initial commit

---
+++ src/main/java/io/github/potassiummc/globalstatistics/GlobalStatistics.java
@@ -0,0 +1,15 @@
+package io.github.potassiummc.globalstatistics;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GlobalStatistics implements ModInitializer {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger("globalstatistics");
+
+    @Override
+    public void onInitialize() {
+    }
+
+}
